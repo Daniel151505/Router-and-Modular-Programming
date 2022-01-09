d8f3a2c740c4937ba6c31f2b1056bc8408e6c57b
Daniel151505 <daniel150520011@gmail.com>
2022-01-08 21:48:48 -0500

    Creating home

--- my-project/src/app/pages/home/home.component.ts
+++ my-project/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+  limit = 10;
+  offset = 0;
+
+  constructor(
+    private productsService: ProductsService,
+  ) { }
 
   ngOnInit(): void {
+    this.productsService.getAll(10, 0).subscribe((data) => {
+      this.products = data;
+      this.offset += this.limit;
+    });
   }
-
 }
